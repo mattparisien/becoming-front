20630892db3f54bf1a5d2ef0de398eb3ba471485
Matt Parisien <63109316+mattparisien@users.noreply.github.com>
2025-11-20 15:36:48 -0500

    S

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,7 +2,7 @@ export default function ContactForm() {
 
     return (
         <div className="w-full h-full flex flex-col items-center justify-center min-h-[calc(var(--screen-minus-header-height)-2rem)]">
-            <a href="mailto:hello@becomingplugins.com" className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-serif font-light mb-10">hello@becomingplugins.com</a>
+            <a href="mailto:hello@becomingplugins.com" className="text-foreground text-[6vw] font-serif font-light mb-10">hello@becomingplugins.com</a>
         </div>
     );
 }
