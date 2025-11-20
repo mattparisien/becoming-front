c30a4a27ddf33cfad9f6df4dd1845b3a01834014
Matt Parisien <63109316+mattparisien@users.noreply.github.com>
2025-11-20 12:54:17 -0500

    S

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import localFont from 'next/font/local';
 import "./globals.css";
-import GridAnimation from '@/components/GridAnimation';
 
 const roobert = localFont({
   src: './assets/fonts/RoobertCollectionVF.woff2',
@@ -43,7 +42,6 @@ export default function RootLayout({
     >
       <body className='font-sans antialiased'>
         {children}
-        {/* <GridAnimation />  */}
       </body>
     </html>
   );
