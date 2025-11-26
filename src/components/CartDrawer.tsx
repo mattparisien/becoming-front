2616613b870e3e8e1bd984313c6b20af10fe9693
Matt Parisien <63109316+mattparisien@users.noreply.github.com>
2025-11-25 20:38:02 -0500

    S

--- src/components/CartDrawer.tsx
+++ src/components/CartDrawer.tsx
@@ -435,7 +435,7 @@ const CartDrawer = ({ isOpen, onClose }: CartDrawerProps) => {
                 </span>
               </div>
               {currentView === 'cart' ? (
-                <Button size="lg" fullWidth onClick={handleProceedToCheckout}>
+                <Button size="lg" fullWidth onClick={handleProceedToCheckout} disabled>
                   {t.checkout}
                 </Button>
               ) : (
